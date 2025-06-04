008cd76edf58f5bc7d3cbdc8cde8c1a32c96cfd8
Neurotech-xmd <onyariallamano@gmail.com>
2025-06-04 21:49:20 +0300

    Update getall.js

--- commandes/getall.js
+++ commandes/getall.js
@@ -18,9 +18,9 @@ ezra({ nomCom: "getallmembers", categorie: 'Group', reaction: "📣" }, async (d
   let membresGroupe = verifGroupe && infosGroupe ? infosGroupe.participants || [] : [];
 
   let tag = `========================\n  
-        🌟 *DML-MD GROUP MEMBERS GIDS* 🌟
+        🌟 *NEURO-MD GROUP MEMBERS GIDS* 🌟
 ========================\n
-> regards DML-TECH®\n\n`;
+> regards NEURO-TECH®\n\n`;
 
   const emoji = ['🦴', '👀', '😮‍💨', '❌', '✔️', '😇', '⚙️', '🔧', '🎊', '😡', '🙏🏿', '⛔️', '$', '😟', '🥵', '🐅'];
   const randomEmoji = emoji[Math.floor(Math.random() * emoji.length)];
@@ -57,7 +57,7 @@ ezra({ nomCom: "tagadmin", categorie: 'Group', reaction: "📣" }, async (dest, 
   let adminsGroupe = membresGroupe.filter(membre => membre.isAdmin);
 
   let tag = `========================\n  
-        🌟 *DML-MD* 🌟
+        🌟 *NEURO-MD* 🌟
 ========================\n
 👥 Group : ${nomGroupe} 🚀 
 👤 Author : *${nomAuteurMessage}* 👋 
